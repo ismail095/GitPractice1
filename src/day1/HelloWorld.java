81fbbd8269bea45ce49b749ddbd465330c7a33c0
ismail095 <ismail.hassab01@gmail.com>
2021-04-24 16:56:08 -0400

    added more work

--- src/day1/HelloWorld.java
+++ src/day1/HelloWorld.java
@@ -5,5 +5,6 @@ public class HelloWorld {
         System.out.println("Hello World");
         System.out.println("Adding GitHub now");
         System.out.println("YA STUFFS ARE ON GITHUB");
+        // adding final wrap up code
     }
 }
